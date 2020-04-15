a3930d9c3e58ded3fb47325baae2dc6482be9815
Bruno Alencar <brunoromero7@gmail.com>
2020-04-14 22:38:43 -0400

    Passing a Department object to DepartmentForm view

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,50 +9,62 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
+public class DepartmentFormController implements Initializable {
 
-public class DepartmentFormController implements Initializable{
-	
+	private Department entity;
+
 	@FXML
 	private TextField txtId;
-	
+
 	@FXML
 	private TextField txtName;
-	
+
 	@FXML
 	private Label labelErrorName;
-	
+
 	@FXML
 	private Button btSave;
-	
+
 	@FXML
 	private Button btCancel;
-	
+
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+
 	@FXML
 	public void onBtSaveAction() {
 		System.out.println("onBtSaveAction");
 	}
-	
+
 	@FXML
 	public void onBtCancelAction() {
 		System.out.println("onBtCancelAction");
 	}
-	
-	
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		initializeNodes();
-		
+
 	}
-	
+
 	private void initializeNodes() {
-		//A linha abaixo faz o txtId aceitar apenas Valor do tipo Integer
+		// A linha abaixo faz o txtId aceitar apenas Valor do tipo Integer
 		Constraints.setTextFieldInteger(txtId);
-		//A linha abaixo faz o txtName receber até 30 caracteres
+		// A linha abaixo faz o txtName receber até 30 caracteres
 		Constraints.setTextFieldMaxLength(txtName, 30);
-		
-		
+
 	}
-	
+
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+
+	}
 
 }
